555c0065427b0c2310c35c492c97efe5e0473437
Klaudia <klaudia134000@wp.pl>
2026-05-20 09:32:23 +0200

    initial commit

---
+++ tdlist.js
@@ -0,0 +1,8 @@
+// we need 3 things to do:
+// 1. load saved data
+// 2. add and update new data to list
+// 3. delete data by clicking x, and update
+
+// const taskInput = document.getElementById("taskInputId")
+// const taskList
+// const addBtn
